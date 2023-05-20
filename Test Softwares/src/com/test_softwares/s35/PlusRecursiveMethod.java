6651b73b55ebeeea49a90df3e262e8121ecde09e
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-20 15:28:59 -0400

    new package

--- Test Softwares/src/com/test_softwares/s35/PlusRecursiveMethod.java
+++ Test Softwares/src/com/test_softwares/s35/PlusRecursiveMethod.java
@@ -2,7 +2,13 @@ package com.test_softwares.s35;
 
 public class PlusRecursiveMethod {
 
-	public static somatorioRecursivo(int N) {
+	public static int somatorioRecursivo(int n) {
+		
+		int soma = n;
+		if (soma == 1) {
+			return soma;
+		}
+		return soma + somatorioRecursivo(n - 1);
 		
 	}
 	
