0e4d47b65dacf8496e765cc55a2e65e9a9e2bf11
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-20 16:26:54 -0400

    update

--- Test Softwares/src/com/test_softwares/s35s2/FibonacciSequenceTest.java
+++ Test Softwares/src/com/test_softwares/s35s2/FibonacciSequenceTest.java
@@ -4,7 +4,15 @@ public class FibonacciSequenceTest {
 
 	public static void main(String[] args) {
 		
-		
+		int x = 1;
+		int y = 0;
+		int auxiliar;
+		for (int i = 0; i < 30; i++) {  // sequência de fibonacci usando um loop normal
+			System.out.println(x);
+			auxiliar = x;
+			x = x + y;
+			y = auxiliar;
+		}
 		
 		
 		
