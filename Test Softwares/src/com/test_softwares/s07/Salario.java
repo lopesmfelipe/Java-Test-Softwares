2af335e53eb11169c963e602604c9b0393b81b17
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-26 23:59:32 -0400

    formatted code

--- Test Softwares/src/com/test_softwares/s07/Salario.java
+++ Test Softwares/src/com/test_softwares/s07/Salario.java
@@ -5,12 +5,13 @@ import java.util.Scanner;
 public class Salario {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("Quanto você ganha por hora?");
 		Scanner scan = new Scanner(System.in);
-		
+
 		System.out.println("Quantas horas você trabalha por mês?");
 		Scanner scan2 = new Scanner(System.in);
+
 	}
 
 }
