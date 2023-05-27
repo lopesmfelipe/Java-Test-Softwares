9f84735e7b821f8c5d318fd23eaec19ebc3a1a56
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-26 23:56:24 -0400

    nova entrada de dados

--- Test Softwares/src/com/test_softwares/s07/Salario.java
+++ Test Softwares/src/com/test_softwares/s07/Salario.java
@@ -1,10 +1,15 @@
 package com.test_softwares.s07;
 
+import java.util.Scanner;
+
 public class Salario {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println();
+		Scanner scan = new Scanner(System.in);
+		
+		
 	}
 
 }
