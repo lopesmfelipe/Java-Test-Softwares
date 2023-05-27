ca2e163e90055888807d636f8873ee66ca6c170b
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-27 00:11:36 -0400

    result output

--- Test Softwares/src/com/test_softwares/s07/Salario.java
+++ Test Softwares/src/com/test_softwares/s07/Salario.java
@@ -6,11 +6,19 @@ public class Salario {
 
 	public static void main(String[] args) {
 
-		System.out.println("Quanto você ganha por hora?");
+		System.out.println("Quanto você faz por hora?");
 		Scanner scan = new Scanner(System.in);
-
+		int ganhoPorHora = scan.nextInt();
+		
+		
 		System.out.println("Quantas horas você trabalha por mês?");
 		Scanner scan2 = new Scanner(System.in);
+		int horasPorMes = scan2.nextInt();
+		
+		int result = ganhoPorHora * horasPorMes;
+		System.out.println("Você faz $" + result + " por mês");
+	
+		
 
 	}
 
