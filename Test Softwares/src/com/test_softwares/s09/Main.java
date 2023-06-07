59739df6ae5bccf12463513093b32dfbdafe442c
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-06-06 23:52:57 -0400

    Update

--- Test Softwares/src/com/test_softwares/s09/Main.java
+++ Test Softwares/src/com/test_softwares/s09/Main.java
@@ -4,11 +4,20 @@ import java.util.Scanner;
 
 public class Main {
 
+	
 	public static void main(String[] args) {
+		Main x = new Main();
 		
 		Scanner scanner01 = new Scanner(System.in);
 		
 		System.out.println("Report the temperature in degrees fahrenheit");
+		
+		System.out.println("The temperature reported is ");
+	}
+	
+	
+	public void conversion() {
+		
 	}
 
 }
