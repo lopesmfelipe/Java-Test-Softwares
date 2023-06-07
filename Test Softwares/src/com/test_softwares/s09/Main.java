01ae1af220f220cacbc13e91dd2c4b228ee70051
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-06-06 23:44:35 -0400

    update

--- Test Softwares/src/com/test_softwares/s09/Main.java
+++ Test Softwares/src/com/test_softwares/s09/Main.java
@@ -1,9 +1,12 @@
 package com.test_softwares.s09;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		Scanner scanner01 = new Scanner(System.in);
 		
 		
 	}
